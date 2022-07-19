9be2055cd6496b6c80ae6c6e251629a869686f35
dnafication <dnafication@users.noreply.github.com>
2022-07-19 13:19:51 +1000

    feat: aws bookmarks

--- cli.js
+++ cli.js
@@ -1,26 +1,75 @@
 #!/usr/bin/env node
-import meow from 'meow';
-import unicornFun from 'unicorn-fun';
+import meow from "meow";
+import open from "open";
+import prompts from "prompts";
+import { ms } from "./lib/bookmarks.js";
 
-const cli = meow(`
+const cli = meow(
+	`
 	Usage
-	  $ unicorn-fun [input]
+	  $ bm [input]
 
 	Options
-	  --postfix  Lorem ipsum  [Default: rainbows]
+	  --in <app name>
+		--incognito
 
 	Examples
-	  $ cli-name
-	  unicorns & rainbows
-	  $ cli-name ponies
-	  ponies & rainbows
-`, {
-	flags: {
-		postfix: {
-			type: 'string',
-			default: 'rainbows'
-		}
+	  $ bm lambda
+	  $ bm ec2 --in firefox --incognito
+	  $ bm dynamo --in "google chrome"
+`,
+	{
+		flags: {
+			in: {
+				type: "string",
+				default: "google chrome",
+			},
+			incognito: {
+				type: "boolean",
+				default: false,
+			},
+		},
 	}
+);
+
+// console.log(cli.input);
+// console.log(cli.flags);
+
+const results = ms.search(cli.input[0], {
+	fields: ["title", "url"],
+	fuzzy: 0.2,
+	prefix: true,
 });
 
-console.log(moduleName(cli.input[0] || 'unicorns', cli.flags));
+const openLink = async (searchResult) => {
+	console.log(
+		`Opening ${searchResult.title} in ${cli.flags.in}. InCognito: ${cli.flags.incognito}`
+	);
+	await open(searchResult.url, {
+		app: {
+			name: cli.flags.in,
+			arguments: cli.flags.incognito ? ["--incognito"] : [],
+		},
+	});
+};
+
+if (results.length > 1) {
+	const answer = await prompts({
+		type: "autocomplete",
+		message: "Which one?",
+		name: "pick",
+		choices: results.map((result) => ({
+			title: result.title,
+			value: result,
+			description: result.description,
+		})),
+	});
+
+	if (answer.pick) {
+		await openLink(answer.pick);
+	}
+}
+
+if (results.length === 1) {
+	await openLink(results[0]);
+}
--- readme.md
+++ readme.md
@@ -1,54 +1,19 @@
-# node-cli-boilerplate
+# bm
 
-> Boilerplate to kickstart creating a Node.js command-line tool
+open my bookmarks from terminal
 
-This is what I use for [my own command-line tools](https://www.npmjs.com/~sindresorhus).
-
-Also check out [`node-module-boilerplate`](https://github.com/sindresorhus/node-module-boilerplate).
-
-## Getting started
-
-**Click the "Use this template" button.**
-
-Alternatively, create a new directory and then run:
+## usage
 
 ```
-$ curl -fsSL https://github.com/sindresorhus/node-cli-boilerplate/archive/main.tar.gz | tar -xz --strip-components=1
-```
+	Usage
+	  $ bm [input]
 
-There's also a [Yeoman generator](https://github.com/sindresorhus/generator-nm).
+	Options
+	  --in <app name>
+		--incognito
 
-
----
-
-**Remove everything from here and above**
-
----
-
-# unicorn-fun-cli
-
-> My awesome command-line tool
-
-## Install
-
-```
-$ npm install --global unicorn-fun-cli
-```
-
-## Usage
-
-```
-$ unicorn-fun --help
-
-  Usage
-    $ unicorn-fun [input]
-
-  Options
-    --postfix  Lorem ipsum  [Default: rainbows]
-
-  Examples
-    $ cli-name
-    unicorns & rainbows
-    $ cli-name ponies
-    ponies & rainbows
+	Examples
+	  $ bm lambda
+	  $ bm ec2 --in firefox --incognito
+	  $ bm dynamo --in "google chrome"
 ```
